40171c0834eb72364d52e7474348f728c244052d
kani-a <allanchagasilva@gmail.com>
2025-09-10 16:30:20 -0300

    safe comit

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput } from 'react-native-web';
 
 export default function App() {
+  
+  const [pesso, setPesso] = useState('');
+  const [altura , setAltura] = useState('')
+  const [result, setReslt] = useState(null)
+  const [massage , setMessage] = useState('')
+
+  const calcular = () => {
+    if(pesso && altura){
+      console.log(pesso/(altura*altura));
+      
+      alert(pesso/(altura*altura))
+      return pesso/(altura*altura)
+    }else{
+      alert("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Calcule seu IMC
+      </Text>
       <StatusBar style="auto" />
+      
+      <View>
+        <TextInput placeholder='pesso'
+        keyboardType='numeric'
+        value={altura}
+        onChange={setAltura}
+        />
+        
+        <TextInput placeholder='altura'
+        keyboardType='numeric'
+        value={pesso}
+        onChange={setPesso}
+        />
+      </View>
+
+      <Button onPress={() => calcular()}title='hihi'>
+        calcular
+      </Button>
     </View>
   );
 }
