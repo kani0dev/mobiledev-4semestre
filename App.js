5e2bcf568efa8e744a46b87a4849a5cd8b1f106c
kani0dev <allanchagasilva@gmail.com>
2025-09-17 17:22:48 -0300

    peguei a versão do Ygera pra usar

--- App.js
+++ App.js
@@ -1,75 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
+import { View, Text, StyleSheet, TextInput, Pressable, Alert } from "react-native";
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, TextInput } from 'react-native-web';
 
 export default function App() {
-  
-  const [pesso, setPesso] = useState('');
-  const [altura , setAltura] = useState('')
-  const [result, setReslt] = useState()
-  const [massage , setMessage] = useState('')
 
-  const calcular = () => {
-    if(pesso && altura){
-      let imc =  pesso.replace(',','.')/(altura.replace(',','.')^2);
-      
-      setReslt(imc.toFixed(2));
-      console.log(imc);
-      console.log(result);
-      
-      
-      switch(imc){
-        case imc < 18.5: setMessage("magro") ;break;
-        case imc > 18.5 && imc <= 24.9: setMessage("normal\npesso: ", imc);break;
-        case imc > 25 && imc <= 29.9: setMessage("gordinho\npesso: ", imc);break;
-        case imc > 30 && imc <= 34.9 : setMessage("Gigantesco\npesso: ", imc);break;
-        default : setMessage("KRLTAMPANDO O SOL MANEKKKKKKKKKK\npesso: ", imc);break;
+  const [peso, setPeso] = useState('');
+
+  const [altura, setAltura] = useState('');
+
+  const [resultado, setResultado] = useState(null);
+
+  const [diag, setDiag] = useState('');
+
+  const calcularIMC = () => {
+
+    if (peso !== '' && altura !== '') {
+      let pesoNum = (peso.replace(",", "."));
+
+      let alturaNum = (altura.replace(",", "."));
+
+      let imc = pesoNum / (alturaNum * alturaNum);
+
+      setResultado(imc.toFixed(2));
+
+      if (imc < 18.5) {
+        setDiag('Magreza');
       }
 
-    }else{
-      alert("tem nao")
+      else if (imc >= 18.5 && imc <= 24.9) {
+        setDiag('Normal');
+      }
+
+      else if (imc >= 25 && imc <= 29.9) {
+        setDiag('Sobrepeso');
+      }
+
+      else if (imc >= 30 && imc <= 39.9) {
+        setDiag('Obesidade');
+      }
+
+      else {
+        setDiag('Obesidade Grave');
+      }
+
+    } else {
+      alert('Por favor preencha o campo de peso e altura');
     }
+
   }
 
-
   return (
-    <View style={styles.container}>
-      <Text>
-        Calcule seu IMC
-      </Text>
-      <StatusBar style="auto" />
-      
+    <View style={styles.app}>
+      <Text style={styles.titulo}>Calcule seu IMC</Text>
+
       <View>
-        <TextInput placeholder='altura'
-        keyboardType='numeric'
-        value={altura}
-        onChangeText={setAltura}
-        />
-        
-        <TextInput placeholder='pesso'
-        keyboardType='numeric'
-        value={pesso}
-        onChangeText={setPesso}
+        <TextInput
+          style={styles.input}
+          placeholder="Digite o peso"
+          keyboardType="numeric"
+          value={peso}
+          onChangeText={setPeso}
         />
       </View>
 
-      <Button onPress={() => calcular()}title='oi o q loco kkkkk'>
-        calcular
-      </Button>
-      <h2>
-        {massage}
-        {pesso}
-      </h2>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite a altura"
+          keyboardType="numeric"
+          value={altura}
+          onChangeText={setAltura}
+        />
+      </View>
+
+      <View>
+        <Pressable
+          style={({ pressed }) => [
+            styles.btn,
+            pressed && { opacity: 0.8, backgroundColor: "#000000" },
+          ]}
+          onPress={calcularIMC}
+        >
+          <Text style={styles.btnText}>Clique aqui</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.textos}>
+        <Text>Resultado: {resultado}</Text>
+        <Text>Diagnostico: {diag}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ccc",
+  },
+  titulo: {
+    fontSize: 30,
+    color: "black",
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 2,
+    width: 300,
+    height: 40,
+    fontSize: 15,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    borderRadius: 20,
+    textAlign: "center",
+    backgroundColor: "white",
+  },
+  btn: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 2,
+    width: 140,
+    height: 40,
+    borderRadius: 10,
+    backgroundColor: "#7DDA58",
+    marginBottom: 30,
+  },
+  btnText: {
+    fontSize: 18,
+    color: "white",
+    fontWeight: "bold",
+  },
+  textos: {
+    alignItems: "center",
   },
 });
